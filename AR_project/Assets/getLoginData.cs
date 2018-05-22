a9da33b5e6a70a6b1b5003a4c120ab61c0f884a9
christineW95 <39514882+christineW95@users.noreply.github.com>
2018-05-22 20:48:16 +0200

    version 7

---
+++ AR_project/Assets/getLoginData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class getLoginData : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    InputField data;
+
+    string username, password;
+
+   public void getData()
+    {
+        Debug.Log(data.text);
+        username = data.text;
+
+        Debug.Log(username);
+    }
+}
