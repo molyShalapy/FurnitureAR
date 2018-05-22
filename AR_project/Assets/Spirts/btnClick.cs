26bc91f612d0e23bc4b85ac6a0772125516d170b
christineW95 <39514882+christineW95@users.noreply.github.com>
2018-05-22 12:22:50 +0200

    pattern v2

---
+++ AR_project/Assets/Spirts/btnClick.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btnClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	public void onButtonClick()
+    {
+        Debug.Log("clicked");
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
