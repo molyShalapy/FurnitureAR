2f63af10c4720567aba6dfa5ad600967cc34bc63
mohammed-yassien15 <39514614+mohammed-yassien15@users.noreply.github.com>
2018-05-23 11:01:34 +0200

    update screen back ground
    the backgroud will be white and the size is suitable

---
+++ AR_project/Assets/loadHomeScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loadHomeScene : MonoBehaviour {
+
+    public void LoadHomeScene()
+    {
+        Debug.Log("loaded");
+        Application.LoadLevel("SideMenueScrene");
+    }
+}
