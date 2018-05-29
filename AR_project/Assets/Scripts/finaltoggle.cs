0ed9cc795aa4fa4cfedc96e371a551ac7a95e99c
christineW95 <39514882+christineW95@users.noreply.github.com>
2018-05-29 13:54:08 +0200

    LoginUI 1.54PM

---
+++ AR_project/Assets/Scripts/finaltoggle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class finaltoggle : MonoBehaviour {
+
+    public ToggleGroup tg;
+    public GameObject obj;
+    // Use this for initialization
+  public  void Start () {
+        Toggle toggle = tg.ActiveToggles().First();
+        obj.GetComponent<Renderer>().material.color = ToColor(toggle.tag);
+    }
+
+  
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public Color ToColor(string color)
+    {
+        return (Color)typeof(Color).GetProperty(color.ToLowerInvariant()).GetValue(null, null);
+    }
+}
