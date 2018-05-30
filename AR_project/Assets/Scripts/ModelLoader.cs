0ee92af9f006caee01bc3528cb34c89164657fc3
christineW95 <39514882+christineW95@users.noreply.github.com>
2018-05-30 23:09:50 +0200

    11.09PM

---
+++ AR_project/Assets/Scripts/ModelLoader.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelLoader : MonoBehaviour {
+
+    public GameObject[] prefabs;
+    private int index=0;
+    private static ModelLoader instance;
+    
+    public static ModelLoader Instance
+    {
+        
+        get
+        {
+            return instance;
+        }
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);                
+        if (!instance)
+        {
+            instance = this;
+        }
+    }
+
+    // Use this for initialization
+    public GameObject GetCurrentObject()
+    {
+        
+        Debug.Log(index);
+        return prefabs[index];
+    }
+    public void SetIndex(int index)
+    {
+         this.index =Mathf.Max(prefabs.Length, index);
+        //this.index = index;
+      //  Debug.Log("ddddddddddddddd " + this.index);
+
+    }
+
+}
