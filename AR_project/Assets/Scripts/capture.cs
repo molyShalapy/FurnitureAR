553b7408c941531a7a6c22a63cbd2a9e26a643de
christineW95 <39514882+christineW95@users.noreply.github.com>
2018-05-31 14:01:05 +0200

    2.00PM

---
+++ AR_project/Assets/Scripts/capture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+//using UnityEngine.Experimental.UIElements;
+using UnityEngine.SceneManagement;
+//using static System.Net.Mime.MediaTypeNames;
+
+public class capture : MonoBehaviour {
+
+
+    public static string imageName = "screenshot.png";
+
+    public void capturePic()
+    {
+        Debug.Log("Captured");
+        ScreenCapture.CaptureScreenshot(imageName);
+        SceneManager.LoadScene("share");
+
+
+    }
+}
---
+++ AR_project/Assets/Scripts/share.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class share : MonoBehaviour {
+    Texture2D screenshotTexture;
+    byte[] data;
+    public   GameObject screenshotPreview;
+    // Use this for initialization
+    void Start () {
+
+        data = File.ReadAllBytes(capture.imageName);
+        Debug.Log("image" + data);
+        screenshotTexture = new Texture2D(Screen.width, Screen.height);
+        Debug.Log(data);
+        screenshotTexture.LoadImage(data);
+
+        // Create a sprite
+        Sprite screenshotSprite = Sprite.Create(screenshotTexture, new Rect(0, 0, Screen.width, Screen.height), new Vector2(0.5f, 0.5f));
+
+        // Set the sprite to the screenshotPreview
+        screenshotPreview.GetComponent<Image>().sprite = screenshotSprite;
+    }
+	
+}
